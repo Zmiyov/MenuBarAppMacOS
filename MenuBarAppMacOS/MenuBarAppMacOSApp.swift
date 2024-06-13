e088301ee4ab8dc2875c64936405d7d5ef957457
Volodymyr <zmiyov88@gmail.com>
2024-06-13 23:58:23 +0300

    1

--- MenuBarAppMacOS/MenuBarAppMacOSApp.swift
+++ MenuBarAppMacOS/MenuBarAppMacOSApp.swift
@@ -9,9 +9,53 @@ import SwiftUI
 
 @main
 struct MenuBarAppMacOSApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+//Making it as pure menu bar app in info.plist
+
+//Setting up Menu Bar Icon and Menu Popover area
+class AppDelegate: NSObject, ObservableObject, NSApplicationDelegate {
+    @Published var statusItem: NSStatusItem?
+    @Published var popover = NSPopover()
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        setupMacMenu()
+    }
+    
+    func setupMacMenu() {
+        //Popover properties
+        popover.animates = true
+        popover.behavior = .transient
+        
+        //Linking SwiftUI View
+        popover.contentViewController = NSViewController()
+        popover.contentViewController?.view = NSHostingView(rootView: Home())
+        
+        //Making it as key window
+        popover.contentViewController?.view.window?.makeKey()
+        
+        //Setting menu bar icon and action
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let menuButton = statusItem?.button {
+            menuButton.image = .init(systemSymbolName: "dollarsign.circle.fill", accessibilityDescription: nil)
+            menuButton.action = #selector(menuButtonAction(sender:))
+        }
+    }
+    
+    @objc func menuButtonAction(sender: AnyObject) {
+        if popover.isShown {
+            popover.performClose(sender)
+        } else {
+            if let menuButton = statusItem?.button {
+                popover.show(relativeTo: menuButton.bounds, of: menuButton, preferredEdge: .minY)
+            }
         }
     }
 }
---
+++ MenuBarAppMacOS/View/Home.swift
@@ -0,0 +1,79 @@
+//
+//  Home.swift
+//  MenuBarAppMacOS
+//
+//  Created by Volodymyr Pysarenko on 13.06.2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    @State var currentTab: String = "Crypto"
+    @Namespace var animation
+    var body: some View {
+        VStack{
+            CustomSegmentedControl()
+                .padding()
+            
+            Spacer()
+            
+            HStack{
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "gearshape.fill")
+                })
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "power")
+                })
+            }
+            .padding(.horizontal)
+            .padding(.vertical, 10)
+            .background(Color.black)
+        }
+        .frame(width: 320, height: 450)
+        .background(Color.bg)
+        .preferredColorScheme(.dark)
+        .buttonStyle(.plain)
+    }
+    
+    @ViewBuilder
+    func CustomSegmentedControl() -> some View {
+        HStack(spacing: 0){
+            
+            ForEach(["Crypto", "Stocks"], id: \.self) { tab in
+                Text(tab)
+                    .fontWeight(currentTab == tab ? .semibold : .regular)
+                    .foregroundStyle(currentTab == tab ? .white : .gray)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 6)
+                    .background{
+                        if currentTab == tab {
+                            RoundedRectangle(cornerRadius: 8, style: .continuous)
+                                .fill(.tab)
+                                .matchedGeometryEffect(id: "TAB", in: animation)
+                        }
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        withAnimation {
+                            currentTab = tab
+                        }
+                    }
+            }
+        }
+        .padding(2)
+        .background{
+            Color.black.clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+        }
+    }
+}
+
+#Preview {
+    Home()
+}
