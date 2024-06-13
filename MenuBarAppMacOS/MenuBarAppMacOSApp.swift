213272003e2262d7f65ebc1f6b87e11525363e11
Volodymyr <zmiyov88@gmail.com>
2024-06-13 22:48:29 +0300

    Initial Commit

---
+++ MenuBarAppMacOS/MenuBarAppMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarAppMacOSApp.swift
+//  MenuBarAppMacOS
+//
+//  Created by Volodymyr Pysarenko on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarAppMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
